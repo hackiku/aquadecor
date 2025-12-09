c837d2303ffdef5188d0953a136156934d41069a
hackiku <ivan@pipewriter.io>
2025-12-09 13:33:15 +0100

    i18n: homepage metadata in (website) layout

--- src/app/[locale]/(website)/layout.tsx
+++ src/app/[locale]/(website)/layout.tsx
@@ -1,12 +1,39 @@
 // src/app/[locale]/(website)/layout.tsx
 
+import { getTranslations } from 'next-intl/server';
 import { Footer } from "~/components/navigation/Footer";
 
-export default function WebsiteLayout({
-	children,
-}: {
+type Props = {
 	children: React.ReactNode;
-}) {
+	params: Promise<{ locale: string }>;
+};
+
+export async function generateMetadata({ params }: Props) {
+	const { locale } = await params;
+	const t = await getTranslations({ locale, namespace: 'home' });
+
+	return {
+		title: {
+			template: '%s | Aquadecor', // Will add this to all child pages
+			default: t('meta.title'),
+		},
+		description: t('meta.description'),
+		openGraph: {
+			title: t('meta.title'),
+			description: t('meta.description'),
+			siteName: 'Aquadecor',
+			locale: locale,
+			type: 'website',
+		},
+		twitter: {
+			card: 'summary_large_image',
+			title: t('meta.title'),
+			description: t('meta.description'),
+		},
+	};
+}
+
+export default function WebsiteLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
 			{children}
